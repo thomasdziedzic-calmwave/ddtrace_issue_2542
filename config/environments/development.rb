c45537c999646a7e03b5777b228d56c5cff4d1eb
Thomas Dziedzic <thomas@calmwave.ai>
2023-01-17 08:15:32 -0600

    workaround

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,8 @@
 require "active_support/core_ext/integer/time"
 
+# REMOVE THIS TO REPRODUCE THE ISSUE
+Datadog.configuration.tracing.log_injection = false
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
